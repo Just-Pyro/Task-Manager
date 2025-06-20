1d24696b2178b3be7fa3dec9d2f27b37e22ea097
Vernon <vpcalma@proweaver.net>
2025-06-20 11:45:30 +0800

    Initial Commit

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div className="p-6 text-blue-600 font-bold">
+            Welcome to your Tailwind + React page!
+        </div>
+    );
+};
+
+export default Home;
