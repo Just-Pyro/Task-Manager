aa9669ff9f3751c31d694b6eeec9135dea6ef8a4
Praise Nacua <praise@email.com>
2025-07-07 20:47:51 +0800

    - Added styled input for Adding tasks

--- resources/js/Pages/MyTasks.jsx
+++ resources/js/Pages/MyTasks.jsx
@@ -14,13 +14,18 @@ const MyTasks = () => {
                 <div className="container max-w-7xl h-full border border-yellow-500 flex">
                     <div className="w-2/3 border border-green-500 flex flex-col p-5">
                         <div className="flex justify-center">
-                            <div className="bg-white rounded-lg shadow-sm w-full">
-                                Tasks here
+                            <div className="bg-white rounded-lg shadow-sm w-full p-5">
+                                <div className="">
+                                    <input type="text" name="create_task" id="create_task" placeholder="Type something..." className="px-1 w-full border-0 focus:border-b-2 border-blue-500 focus:ring-0" />
+                                    <hr />
+
+                                    <div className="h-64"></div>
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className="w-1/3 border border-blue-500 flex justify-center p-5">
-                        <div className="bg-white rounded-lg shadow-sm w-full">
+                        <div className="bg-white rounded-lg shadow-sm w-full p-5">
                             Filters Here
                         </div>
                     </div>
